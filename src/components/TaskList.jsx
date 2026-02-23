375e58cb911998013e5562275232c85d17edc357
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-02-23 01:13:47 -0500

    I created components folder and added files

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,1 +1,47 @@
 
+
+import { useState } from 'react';
+
+export default function TaskList({ tasks, onChangeTask, onDeleteTask }) {
+  return (
+    <ul className='tasklist'>
+      {tasks.map(task => (
+        <li key={task.id}>
+          <Task task={task} onChange={onChangeTask} onDelete={onDeleteTask} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Task({ task, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    
+    <>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={e => onChange({ ...task, done: e.target.checked })}
+      />
+      {isEditing ? (
+        <input
+          value={task.text}
+          onChange={e => onChange({ ...task, text: e.target.value })}
+        />
+      ) : (
+        <span>{task.text}</span>
+      )}
+      
+      <button onClick={() => setIsEditing(!isEditing)}>
+        {isEditing ? 'Save' : 'Edit'}
+      </button>
+    
+      <button onClick={() => onDelete(task.id)} disabled={!task.done}>Delete</button>
+      
+    </>
+    
+    
+  );
+}
